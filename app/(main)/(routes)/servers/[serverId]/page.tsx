0285ba364cb2b4a87e7ffed9eee7776ce29d895b
Dev Bulchandani <devbulchandani8@gmail.com>
2024-09-24 12:36:05 +0530

    Channel Id page initialized

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,52 @@
-const ServerIdPage = () => {
-    return (
-        <div>ServerIdPage</div>
-    )
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
 }
 
-export default ServerIdPage
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return <RedirectToSignIn />
+    }
+    console.log(profile);
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== 'general') {
+        return null;
+    }
+
+    console.log("General Page Id: " + initialChannel?.id);
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+}
+
+export default ServerIdPage;
